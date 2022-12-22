a7a5d5efd1c5b82b2bfaecb5b7ada803f1b2d9a1
Erik White <erik.white@griffeye.com>
2022-12-22 21:01:21 +0100

    Update whisper params

--- src/libvx.c
+++ src/libvx.c
@@ -1573,6 +1573,9 @@ vx_error vx_frame_transfer_audio_data(vx_video* video, AVFrame* av_frame, vx_fra
 			params.language = "en";
 			params.n_threads = 8;
 			params.duration_ms = 0; // Use all the provided samples
+			params.no_context = true;
+			params.max_tokens = 32;
+			params.audio_ctx = 0;
 
 			char* sys_info = whisper_print_system_info();
 			printf("%s", sys_info);
